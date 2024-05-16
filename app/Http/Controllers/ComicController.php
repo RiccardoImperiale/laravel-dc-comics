1bfe51a01d5066d9827b18d8aad03452766d7b58
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-05-16 12:28:04 +0200

    allow mass assignment on Comic model

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -31,15 +31,7 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         $data = $request->all();
-        $newComic = new Comic();
-        $newComic->title = $data['title'];
-        $newComic->description = $data['description'];
-        $newComic->thumb = $data['thumb'];
-        $newComic->price = $data['price'];
-        $newComic->series = $data['series'];
-        $newComic->sale_date = $data['sale_date'];
-        $newComic->type = $data['type'];
-        $newComic->save();
+        Comic::create($data);
 
         return to_route('comics.index');
     }
